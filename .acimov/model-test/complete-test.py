e77e62a74edbdb64d6333a5af4b453da16bd75ef
NicoRobertIn <144037080+NicoRobertIn@users.noreply.github.com>
2023-11-06 18:55:19 +0100

    another diagnose for the dev name

--- .acimov/model-test/complete-test.py
+++ .acimov/model-test/complete-test.py
@@ -49,13 +49,13 @@ names = [
     if item.startswith("--dev=")
 ]
 
-print(args)
-print(names)
-
 skip_pass = "--skip-pass" in args
 tested_only = "--tested-only" in args
 
 dev = names[0] if len(names) > 0 else DEV_USERNAME
+
+print(names[0], len(names), DEV_USERNAME, dev)
+
 mode = modes[0] if len(modes) > 0 else "manual"
 
 report = prepare_graph()
